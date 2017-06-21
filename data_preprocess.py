6d19b6d9e7da91cd0a9c3a249af696d6c6b364c9
QuantumLiu <liuyiliang100@sina.com>
2017-06-21 11:35:33 +0800

    Add files via upload

--- data_preprocess.py
+++ data_preprocess.py
@@ -8,8 +8,11 @@ Created on Mon Jun 19 21:27:21 2017
 import jieba
 import numpy as np
 import h5py
-def get_utext(news_list):
-    return ''.join(['*&'+n.get('title','').strip()+'&*'+'*$'+n.get('abstract','').strip()+'$*' for n in news_list])
+def num_sub(text):
+    r_date=r'((\d{4}|\d{2})(-|/|.)\d{1,2}\3\d{1,2})|(\d{4}年\d{1,2}月\d{1,2}日)|(\d{1,2}月\d{1,2}日)|(\d{1,2}日)'
+    r_num=r'
+    def get_utext(news_list):
+    return ''.join([n.get('title','')+n.get('abstract','') for n in news_list])
 def cut(utext):
     jieba.enable_parallel(32)
     words=jieba.lcut(utext)
